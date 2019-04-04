0ee26342618696190e108d7634c535c48349102a
Kishore Kumar G <kishorekumar080297@gmail.com>
2019-04-04 23:10:22 +0530

    Added Intro Component

---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,16 @@
+/*Created by Kishore Kumar Govindaradjou on 4/4/2019 */
+import React, {Component} from 'react';
+
+const Intro = (props) => (
+  <p class="App-intro">
+    {props.message}
+  </p>
+);
+
+export default Intro;
+
+/*
+class Intro extends Component {
+  <Intro/ >
+}
+*/
--- src/index.js
+++ src/index.js
@@ -4,18 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const getCurrentDate = () => {
-  const currentDate = new Date();
-  return currentDate.toDateString();
-}
-const a = getCurrentDate();
-const greeting = <h1>Hello World! | {getCurrentDate()}</h1>
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(greeting, document.getElementById('root'));
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
