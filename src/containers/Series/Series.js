67cb105905fe6b9c02be1b77489c94767db6376d
Kishore Kumar G <kishorekumar080297@gmail.com>
2019-04-12 01:23:27 +0530

    Added conditional rendering

--- src/containers/Series/Series.js
+++ src/containers/Series/Series.js
@@ -4,7 +4,9 @@ import SeriesList from '../../components/SeriesList/SeriesList'
 class Series extends Component {
 
   state = {
-    series: []
+    series: [],
+    seriesName: '',
+    isFetching: false
   }
 
 
@@ -22,23 +24,47 @@ class Series extends Component {
     //}
 
   onSeriesInputChange = e => {
+    this.setState({seriesName: e.target.value, isFetching: true });
 
     fetch(`https://api.tvmaze.com/search/shows?q=${e.target.value}`)
     .then((response) => response.json())
-    .then(json => this.setState ({series: json}))
+    .then(json => this.setState ({series: json, isFetching: false}))
 
     //console.log(e);
     //console.log(e.target.value);
   }
 
   render() {
+    const {series, seriesName, isFetching} = this.state;
+
     return (
       <div>
-        The series length is - {this.state.series.length}
         <div>
-          <input type="text" onChange={this.onSeriesInputChange} />
+          <input
+            value={seriesName}
+            type="text"
+            onChange={this.onSeriesInputChange} />
         </div>
-        <SeriesList list = {this.state.series}/>
+        {
+          series.length === 0 && seriesName.trim() === ''
+          &&
+          <p>Please enter series name into the input</p>
+        }
+        {
+          series.length === 0 && seriesName.trim() !== ''
+          &&
+          <p>No TV series found with this name</p>
+        }
+        {
+          isFetching === true
+          &&
+          <p>Loading...</p>
+        }
+        {
+          isFetching === false
+          &&
+          <SeriesList list = {this.state.series} />
+        }
       </div>
       )
   }
